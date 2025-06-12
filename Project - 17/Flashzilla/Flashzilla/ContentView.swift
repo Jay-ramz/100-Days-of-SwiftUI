972c4f21d47687e716e8c479dd1ea2096b90dbd1
Jay Ramirez <jayrams18@me.com>
2025-06-11 21:21:24 -0500

    Adding to FlashZilla - New Project

--- Project - 17/Flashzilla/Flashzilla/ContentView.swift
+++ Project - 17/Flashzilla/Flashzilla/ContentView.swift
@@ -7,17 +7,100 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
     }
 }
+
+struct ContentView: View {
+    
+    @State private var cards = DataManager.load()
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @State private var timeRemaining = 100
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    
+    @Environment(\.accessibilityVoiceOverEnabled) var accessibilityVoiceOverEnabled
+    
+    @State private var showingEditScreen = false
+    
+    var body: some View {
+        ZStack {
+            Color(.yellow)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
+                ZStack {
+                    ForEach(cards) { card in
+                        if let index = cards.firstIndex(where: { $0.id == card.id }) {
+                            CardView(card: card, removal: {
+                                removeCard(at: index)
+                            }, sendedBack: {
+                                sendCardBack(at: index)
+                            })
+                            .stacked(at: index, in: cards.count)
+                            .allowsHitTesting(index == cards.count - 1)
+                            .accessibilityHidden(index < cards.count - 1)
+                        }
+                    }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
+            }
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showingEditScreen = true
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                }
+                
+                Spacer()
+            }
+            .foregroundStyle(.white)
+            .font(.largeTitle)
+            .padding()
+        }
+    }
+}
+    .onReceive(timer) { time in
+        guard isActive else { return }
+        
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+        }
+    }
+    .onChange(of: scenePHase) {
+        if scenephase == .active {
+            
+        }
+    }
 
 #Preview {
     ContentView()
