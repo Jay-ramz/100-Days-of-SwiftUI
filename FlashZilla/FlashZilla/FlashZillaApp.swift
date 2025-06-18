0456b8a24b7c7adb4c5b5c7fd50d794d85ad2b82
Jay Ramirez <jayrams18@me.com>
2025-06-17 20:41:26 -0500

    no message

---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Jay Ramirez on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
